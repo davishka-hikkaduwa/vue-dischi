0f48505c91ac4a21a0f915973bb44c8f04103c50
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-09-28 09:28:54 +0200

    dropdown

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
-Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
